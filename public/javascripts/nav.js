384a7e6c020600dd8b46eda83a8584cbd9a8aa9e
feydor <vreyes85739@outlook.com>
2021-02-02 18:04:27 -0700

    Fixed hamburger menu on mobile; fixed nav animation

---
+++ public/javascripts/nav.js
@@ -0,0 +1,53 @@
+/**
+ * javascript for nav elements of views/header
+ */
+(function() {
+  "use strict"
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = document.getElementById("header");
+  if (selectHeader) {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 50) {
+        selectHeader.classList.add('header-scrolled');
+      } else {
+        selectHeader.classList.remove('header-scrolled');
+      }
+    });
+  }
+  /**
+   * Mobile nav toggle (open hamburger menu)
+   */
+  let navbar = document.getElementById("navbar");
+  let mobileNavbarToggle = document.querySelector(".mobile-nav-toggle");
+  let dropdown = document.getElementById("dropdownMenu");
+  if (mobileNavbarToggle) {
+    mobileNavbarToggle.addEventListener("click", () => {
+      navbar.classList.toggle("toggled");
+      mobileNavbarToggle.classList.toggle("bi-list");
+      mobileNavbarToggle.classList.toggle("bi-x");
+
+      // popout sidebar
+      let mobileSidebar = document.querySelector(".mobile-sidebar");
+      if (mobileSidebar) {
+        mobileSidebar.classList.toggle("show");
+      }
+
+    });
+  }
+
+   /**
+   * Mobile nav dropdowns activate
+   */
+  
+  if (navbar) {
+    navbar.addEventListener("click", (event) => {
+      if (navbar.classList.contains("navbar-mobile")) {
+        event.preventDefault(); 
+        navbar.firstChild.classList.toggle("dropdown-active");
+      }    
+    });
+  }
+})();
